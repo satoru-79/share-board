a647eb3573c58861c1d6abb5f47a89fc76b6bed1
矢島聖琉 <yajimaseiryu@yajimaseiryunoMacBook-Air.local>
2024-05-29 22:09:18 +0900

    complete app

--- src/firebase.js
+++ src/firebase.js
@@ -5,13 +5,13 @@ import { getFirestore } from 'firebase/firestore'
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAkqyJijatD2PNM0l6Dl7YHu7Z51X7ZOdE",
-  authDomain: "shareboard-26307.firebaseapp.com",
-  projectId: "shareboard-26307",
-  storageBucket: "shareboard-26307.appspot.com",
-  messagingSenderId: "588891113230",
-  appId: "1:588891113230:web:3f1078163a8d8b57f3f95f",
-  measurementId: "G-HR1HRKXX0G"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain:        process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId:         process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket:     process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId:             process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId:     process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 
